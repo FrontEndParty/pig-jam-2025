af4deb0c9c4ecc5d7f740c14f44ad997aec62b56
IgorTheFixer <cjeanphilippe95@gmail.com>
2025-10-19 15:16:33 -0500

    created obstacle class and gnome object

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,13 +1,25 @@
 import Player from '../objects/Player';
 import { BaseScene } from './BaseScene';
-import { Cop } from '../objects/Cop'; // 1. Import the new Cop class
+import { Cop } from '../objects/Cop';
+import { BaseObstacle } from '../objects/BaseObstacle';
+import { Gnome } from '../objects/Gnome';
+
+// --- HELPER OBSTACLE CLASS ---
+// In a larger project, you'd move these to their own files (e.g., src/objects/Cone.ts)
+class Cone extends BaseObstacle {
+  constructor(scene: BaseScene, x: number, y: number) {
+    super(scene, x, y, 'cone', 5);
+  }
+}
+// -----------------------------
 
 export class Game extends BaseScene
 {
   private _camera: Phaser.Cameras.Scene2D.Camera;
   private _background: Phaser.GameObjects.Image;
   private _msg_text : Phaser.GameObjects.Text;
-  private _player: Player
+  private _player: Player;
+  private _obstacles: Phaser.GameObjects.Group; 
 
   constructor () {
     super('Game');
@@ -25,27 +37,46 @@ export class Game extends BaseScene
         stroke: '#000000', strokeThickness: 8,
         align: 'center'
     });
-
     this._msg_text.setOrigin(0.5);
 
-    // Create the player instance
     this._player = new Player(this, this.scale.width / 2, this.scale.height / 2);
-
-    // --- NEW CODE ADDED HERE ---
-
-    // 2. Create a new Cop instance and place it in the scene
     const cop = new Cop(this, 150, 150);
-
-    // 3. Activate the collision that triggers the game over
     cop.setupCollision(this._player);
 
-    // --- END OF NEW CODE ---
+    this._obstacles = this.add.group({ runChildUpdate: true });
+
+    this.time.addEvent({
+      delay: 1500, 
+      callback: this.spawnObstacle,
+      callbackScope: this,
+      loop: true,
+    });
 
     this.listenForEvents();
   }
 
+  /**
+   * Spawns a random obstacle from a predefined list of obstacle types.
+   * This method is easily scalable by adding new classes to the `obstacleTypes` array.
+   */
+  private spawnObstacle(): void {
+    const obstacleTypes = [Cone, Gnome]; // <-- To add more obstacles, just add their class name here!
+    
+    const spawnX = this.scale.width + 100;
+    const spawnY = this.scale.height - 60; 
+
+    // 1. Pick a random class from the array
+    const RandomObstacleClass = Phaser.Math.RND.pick(obstacleTypes);
+
+    // 2. Create an instance of that class
+    const obstacle = new RandomObstacleClass(this, spawnX, spawnY);
+
+    // 3. Add it to the group, which will manage its update and destruction
+    this._obstacles.add(obstacle);
+  }
+
   public update (_time: number, _delta: number): void {
-    this._player.update()
+    this._player.update();
   }
 
   private listenForEvents () {
